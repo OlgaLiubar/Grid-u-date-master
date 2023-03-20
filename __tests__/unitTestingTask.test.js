3c6d5a846521c71b9f081c086973caf1e4d1d0a4
Olga Liubar <olgaliubar@yandex.ru>
2023-03-20 20:30:38 +0100

    refactor using parameterized tests

--- __tests__/unitTestingTask.test.js
+++ __tests__/unitTestingTask.test.js
@@ -1,12 +1,38 @@
-// @ts-nocheck
 const unitTestingTask = require("../unitTestingTask");
 const timezonedDate = require('timezoned-date');
 
-describe('unitTestingTask', () => {
+const date = new Date("September 17, 1989 09:07:05:03");
+const UtcDate = timezonedDate.makeConstructor(0);
 
-  beforeEach(() => {
-    date = new Date("September 17, 1989 09:07:05:03");
-    UtcDate = timezonedDate.makeConstructor(0);
+describe('unitTestingTask', () => {
+  it.each([
+      ['YYYY', date, '1989'],
+      ['YY', date, '89'],
+      ['MMMM', date, 'September'],
+      ['MMM', date, 'Sep'],
+      ['MM', date, '09'], // returns ISO8601-compatible number of the month
+      ['M', date, '9'], // returns the number of the month in year without zero-padding
+      ['DDD', date, 'Sunday'], //returns the full name of the day
+      ['DD', date, 'Sun'], // returns the short name of the day
+      ['D', date, 'Su'], // returns the two-letters name of the day
+      ['dd', new Date("September 7, 1989 09:07:21:20"), '07'], //returns zero-padded number of day in month
+      ['d', date, '17'], // returns the number of the day in month
+      ['HH', date, '09'], // returns zero-padded hour in 24-hr format
+      ['H', date, '9'], // returns the hour in 24-hr format
+      ['hh', date, '09'], // returns the hour in 24-hr format
+      ['h', date, '9'], // returns the hour in 12-hr format
+      ['mm', date, '07'], // returns zero-padded minutes
+      ['m', date, '7'], // returns minutes
+      ['ss', date, '05'], // returns zero-padded seconds
+      ['s', date, '5'], // returns seconds
+      ['ff', date, '003'], // returns zero-padded milliseconds
+      ['f', date, '3'], // returns milliseconds
+      ['A', new Date("September 17, 1989 20:00:01:205"), 'PM'], // returns AM/PM
+      ['a', date, 'am'], // returns am/pm
+      ['ZZ', new UtcDate(), '+0000'], // returns time-zone in ISO8601-compatible basic format
+      ['Z', new UtcDate(), '+00:00'], // returns time-zone in ISO8601-compatible basic format
+  ])('it properly formats the date when passed format string %p and current date %p expecting %p', (format, date, result) => {
+      expect(unitTestingTask(format, date)).toBe(result);
   });
 
   test('it throws an error if format argument is not a string', () => {
@@ -19,183 +45,29 @@ describe('unitTestingTask', () => {
     }).toThrow('Argument `date` must be instance of Date or Unix Timestamp or ISODate String');
   });
 
-  test('it returns the 4 digits of the year if the format string is "YYYY"', () => {
-    const format = 'YYYY';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('1989');
-  });
-
-  test('it returns the last 2 digits of the year if the format string is "YY"', () => {
-    const format = 'YY';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('89');
-  });
-
-  test('it returns full name of the month if the format string is "MMMM"', () => {
-    const format = 'MMMM';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('September');
-  });
-
-  test('it returns short name of the month if the format string is "MMM"', () => {
-    const format = 'MMM';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('Sep');
-  });
-
-  test('it returns ISO8601-compatible number of the month in year if the format string is "MM"', () => {
-    const format = 'MM';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('09');
-  });
-
-  test('it returns the number of the month in year without zero-padding if the format string is "M"', () => {
-    const format = 'M';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('9');
-  });
-
-  test('it returns the full name of the day if the format string is "DDD"', () => {
-    const format = 'DDD';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('Sunday');
-  });
-
-  test('it returns the short name of the day if the format string is "DD"', () => {
-    const format = 'DD';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('Sun');
-  });
-
-  test('it returns the two-letters name of the day if the format string is "D"', () => {
-    const format = 'D';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('Su');
-  });
-
-  test('it returns zero-padded number of day in month if the format string is "dd"', () => {
-    const format = 'dd';
-    const date = new Date("September 7, 1989 09:07:21:20");
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('07');
-  });
-
-  test('it returns the number of the day in month if the format string is "d"', () => {
-    const format = 'd';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('17');
-  });
-
-  test('it returns zero-padded hour in 24-hr format if the format string is "HH"', () => {
-    const format = 'HH';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('09');
-  });
-
-  test('it returns the hour in 24-hr format if the format string is "H"', () => {
-    const format = 'H';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('9');
-  });
-
-  test('it returns zero-padded hour in 12-hr format if the format string is "hh"', () => {
-    const format = 'hh';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('09');
-  });
-
-  test('it returns the hour in 12-hr format if the format string is "h"', () => {
-    const format = 'h';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('9');
-  });
-
-  test('it returns zero-padded minutes if the format string is "mm"', () => {
-    const format = 'mm';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('07');
-  });
-
-  test('it returns the minutes if the format string is "m"', () => {
-    const format = 'm';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('7');
-  });
-
-  test('it returns zero-padded seconds if the format string is "ss"', () => {
-    const format = 'ss';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('05');
-  });
-
-  test('it returns the seconds if the format string is "s"', () => {
-    const format = 's';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('5');
-  });
-
-  test('it returns zero-padded milliseconds if the format string is "ff"', () => {
-    const format = 'ff';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('003');
-  });
-
-  test('it returns the milliseconds if the format string is "f"', () => {
-    const format = 'f';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('3');
-  });
-
-  test('it returns AM/PM if format is A', () => {
-    const format = 'A';
-    const date = new Date('September 17, 1989 20:00:01:205');
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('PM');
+  test('unitTestingTask.formatters returns a list of custom formats from predefined formatters storage', () => {
+    unitTestingTask.register('longDate', 'd MMMM');
+    unitTestingTask.register('longDateAndTime', {
+      'en': 'MMMM d, h:mma',
+      'default': 'd MMMM, HH:mm'
+    });
+    const formatsList = unitTestingTask.formatters();
+    expect(formatsList).toEqual(expect.arrayContaining(['longDate', 'longDateAndTime']));
   });
   
-  test('it returns am/pm if format is a', () => {
-    const format = 'a';
-    const formattedDate = unitTestingTask(format, date);
-    expect(formattedDate).toBe('am');
-  });
-
-  test('it returns time-zone in ISO8601-compatible basic format if format is ZZ', () => {
-    const format = 'ZZ';
-    const formattedDate = unitTestingTask(format, new UtcDate());
-    expect(formattedDate).toBe('+0000');
+  test('unitTestingTask.register returns formatting function which formats date by calling unitTestingTask', () => {
+    unitTestingTask.register('longDate', 'd MMMM');
+    const formattedDate = unitTestingTask('longDate', date);
+  
+    expect(formattedDate).toBe('17 September');
   });
   
-  test('it returns time-zone in ISO8601-compatible extended format if format is Z', () => {
-    const format = 'Z';
-    const formattedDate = unitTestingTask(format, new UtcDate());
-    expect(formattedDate).toBe('+00:00');
+  test('unitTestingTask.noConflict returns unitTestingTask itself', () => {
+    const result = unitTestingTask.noConflict();
+    
+    expect(result).toBe(unitTestingTask);
   });
-});
 
-test('unitTestingTask.formatters returns a list of custom formats from predefined formatters storage', () => {
-  unitTestingTask.register('longDate', 'd MMMM');
-  unitTestingTask.register('longDateAndTime', {
-    'en': 'MMMM d, h:mma',
-    'default': 'd MMMM, HH:mm'
-  });
-  const formatsList = unitTestingTask.formatters();
-  expect(formatsList).toEqual(expect.arrayContaining(['longDate', 'longDateAndTime']));
-});
-
-test('unitTestingTask.register returns formatting function which formats date by calling unitTestingTask', () => {
-  unitTestingTask.register('longDate', 'd MMMM');
-  const formattedDate = unitTestingTask('longDate', date);
-
-  expect(formattedDate).toBe('17 September');
-});
-
-test('unitTestingTask.noConflict returns unitTestingTask itself', () => {
-  const result = unitTestingTask.noConflict();
-  
-  expect(result).toBe(unitTestingTask);
-});
-
-describe('unitTestingTask.lang function', () => {
   test('it returns current language if the lang argument is not specified', () => {
     const currentLanguage = unitTestingTask.lang();
     expect(currentLanguage).toEqual('en'); 
